52441027398c5a85b0974648df34e81ec611b9a8
pogore <pogorelskaya.evelina@yandex.ru>
2023-08-05 18:13:19 +1000

    first commit

--- app_advertisment/admin.py
+++ app_advertisment/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Advertisement
+from django.db.models.query import QuerySet
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'text', 'price', 'user', 'auction', 'created_date', 'updated_date']
+    list_filter = ['auction', 'date']
+    actions = ['make_action_false', 'make_action_true']
+
+    fieldsets = (
+        (
+            'Общее',
+            {
+                "fields":('title', 'text')
+            }
+        ),
+        (
+            'Финансы',
+            {
+                "fields": ('price', 'auction'),
+                "classes": ['collapse']
+            }
+        )
+
+    )
+
+    @admin.action(description="убрать торг")
+    def make_action_false(self, request, queryset:QuerySet):
+        print(queryset, type(queryset))
+        queryset.update(auction=False)
+
+    @admin.action(description="Добавить торг")
+    def make_action_true(self, request, queryset:QuerySet):
+        print(queryset, type(queryset))
+        queryset.update(auction=True)
 
 # Register your models here.
+admin.site.register(Advertisement, AdvertisementAdmin)
--- app_advertisment/models.py
+++ app_advertisment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
@@ -8,9 +9,35 @@ class Advertisement(models.Model):
     text = models.TextField("текст")
     price = models.FloatField("цена")
     user = models.CharField("пользователь", max_length=126) # пользователь (пока что просто имя)
-    date = models.DateField("дата", auto_now_add=True)
+    date = models.DateTimeField("дата", auto_now_add=True)
+    updated_at = models.DateTimeField("дата обновления", auto_now=True)
+    auction = models.BooleanField("торг", help_text="Возможен торг или нет", default=False)
     class Meta:
         db_table = "Advertisement"
+        verbose_name = 'Список объявлений'
+        verbose_name_plural = 'Список объявлений'
 
     def __str__(self):
         return f'< Advertisement: Advertisement(id={self.pk}, title={self.title}, price = {self.price}) >.'
+
+    @admin.display(description="дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.date.date() == timezone.now().date():
+            create_time = self.date.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color:green; font-weigth:bold;">Сегодня в {} </span>',create_time
+            )
+        return self.date.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description="дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color:blue; font-weigth:bold;">Сегодня в {} </span>',updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
